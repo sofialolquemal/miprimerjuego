70352a62aa396f96cf55b99ded7b8a07bb8a12e5
sofialolquemal <sofiainzunza@insucoabg.cl>
2025-11-11 09:34:53 -0300

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,18 +1,25 @@
 import pygame #importamos la libreria
-pygame.init()
+pygame.init() #Inicializar programa
 
-WIDTH=800 #800 pixeles de ancho
-HEIGHT=600 #600 pixeles de alto
-screen= pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("mi primer jueguito")
+ANCHO=800 #800 pixeles de ancho
+ALTO=600 #600 pixeles de alto
+screen= pygame.display.set_mode((ANCHO, ALTO)) #la libreria hace que se reconozcan
+#como funciones para abrir una ventana
 
-runing = True 
-while runing:
+
+pygame.display.set_caption("Click fruit")
+
+
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #hacer clic en cerrar ventana
-            runing = False #cuando eso pasa se cierra
+            running = False #cuando eso pasa se cierra
     pygame.display.flip() #se guardan los cambios hechos
 pygame.quit() #asi se cierra bien la ventana
+#una variable esta en verdadero (ventana abierta) todo el rato hasta que
+#al apretar la equis, se vuelve falsa y se cierra
 
-#intentodepruebaparagithub
+screen.fill((134,2,225))
+pygame.display.flip()
